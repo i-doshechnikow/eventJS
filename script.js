71495ccde732b208c322c7eed9dcf3eaafe4581d
i-doshechnikow <77616522+i-doshechnikow@users.noreply.github.com>
2021-02-05 17:55:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const btns = document.querySelectorAll('button'),
+    overlay = document.querySelector('.overlay');
+
+// btn.onclick = function () {
+//     alert('click');
+// }
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+// let i = 0;
+
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+// btn.addEventListener('click', (e) => {
+//     // console.log(e.target);
+//     e.target.remove();
+//     // console.log('click');
+
+// });
+const link = document.querySelector('a');
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log(event.target);
+});
